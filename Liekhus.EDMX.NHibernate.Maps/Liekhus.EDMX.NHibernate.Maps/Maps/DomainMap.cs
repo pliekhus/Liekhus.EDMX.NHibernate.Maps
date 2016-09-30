b32654a5e0eea96b73a1125f5c42d3762e853132
Patrick Liekhus <pliekhus@lansingtradegroup.com>
2016-09-29 21:31:53 -0500

    change to path

--- Liekhus.EDMX.NHibernate.Maps/Liekhus.EDMX.NHibernate.Maps/Maps/DomainMap.cs
+++ Liekhus.EDMX.NHibernate.Maps/Liekhus.EDMX.NHibernate.Maps/Maps/DomainMap.cs
@@ -1,1 +1,1 @@
-﻿// Either the ..\..\Lansing.LegacyInternational.Domain\DomainModel.edmx was not found or it's not a V3 EDMX File. You must change this in the T4 Template file.
+﻿// Either the ..\..\{PATH}\DomainModel.edmx was not found or it's not a V3 EDMX File. You must change this in the T4 Template file.
